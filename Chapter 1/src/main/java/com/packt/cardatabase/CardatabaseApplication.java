19ae230f2ce335713c6df0ad282e9cfc6b49df20
juhahinkula <hinkula@gmail.com>
2018-04-27 11:36:14 +0300

    first chapter added

---
+++ Chapter 1/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.packt.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+}

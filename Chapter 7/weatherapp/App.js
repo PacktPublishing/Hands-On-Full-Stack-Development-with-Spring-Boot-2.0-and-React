53811b264cdb8814056d6dcd59404289b2aef165
juhahinkula <hinkula@gmail.com>
2018-05-11 20:30:04 +0300

    Chapter 7 added

---
+++ Chapter 7/weatherapp/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {temp: 0, desc: '', icon: ''}
+  }
+
+  componentDidMount() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=c36b03a963176b9a639859e6cf279299')
+  	.then(response => response.json()) 
+  	.then(responseData => {
+    	this.setState({ 
+   	    temp: responseData.main.temp,
+        desc: responseData.weather[0].description,
+        icon: responseData.weather[0].icon          
+ 	    }); 
+	  });
+  }
+  
+  render() {
+    const imgSrc = 'http://openweathermap.org/img/w/' + this.state.icon + '.png';
+
+    return (
+      <div className="App">
+        <p>Temperature: {this.state.temp}</p>
+        <p>Description: {this.state.desc}</p>
+        <img src={imgSrc} />
+      </div>
+    );
+  }
+}
+
+export default App;

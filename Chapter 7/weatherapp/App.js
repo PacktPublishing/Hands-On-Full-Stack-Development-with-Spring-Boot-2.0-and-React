4c06eaeb2c6d5c36ba50ae67181813c8e0d02a9b
juhahinkula <hinkula@gmail.com>
2018-05-12 18:55:05 +0300

    refactoring

--- Chapter 7/weatherapp/App.js
+++ Chapter 7/weatherapp/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {temp: 0, desc: '', icon: ''}
+    this.state = {temp: 0, desc: '', icon: '', loading: true}
   }
 
   componentDidMount() {
@@ -14,21 +14,28 @@ class App extends Component {
     	this.setState({ 
    	    temp: responseData.main.temp,
         desc: responseData.weather[0].description,
-        icon: responseData.weather[0].icon          
- 	    }); 
-	  });
+        icon: responseData.weather[0].icon,   
+        loading: false       
+       })
+     })
+     .catch(err => console.error(err)); 
   }
   
   render() {
-    const imgSrc = 'http://openweathermap.org/img/w/' + this.state.icon + '.png';
+    const imgSrc = `http://openweathermap.org/img/w/'${this.state.icon}.png`;
 
-    return (
-      <div className="App">
-        <p>Temperature: {this.state.temp}</p>
-        <p>Description: {this.state.desc}</p>
-        <img src={imgSrc} />
-      </div>
-    );
+    if (this.state.loading) {
+     return  <p>Loading</p>;
+    }
+    else {
+      return (
+        <div className="App">
+          <p>Temperature: {this.state.temp} &#8451;</p>
+          <p>Description: {this.state.desc}</p>
+          <img src={imgSrc} alt="Weather icon" />
+        </div>
+      );
+    }
   }
 }
 

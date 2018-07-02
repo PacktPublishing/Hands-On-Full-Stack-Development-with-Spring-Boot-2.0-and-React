2514ba0ea43cf80a4c30bffcdd942b7744bed22b
Juha Hinkula <hinkula@gmail.com>
2018-07-02 17:28:36 +0300

    Update Car.java
    Missing import added

--- Chapter02/src/main/java/com/packt/cardatabase/domain/Car.java
+++ Chapter02/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
 
 @Entity
 public class Car {
